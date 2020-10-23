a0e0aa473f194632ee2d05aa739888d1b1c91a5e
B. Endres <endres@systopia.de>
2020-10-23 16:15:12 +0200

    creating session entity

---
+++ CRM/Remoteevent/BAO/Session.php
@@ -0,0 +1,26 @@
+<?php
+use CRM_Remoteevent_ExtensionUtil as E;
+
+class CRM_Remoteevent_BAO_Session extends CRM_Remoteevent_DAO_Session {
+
+  /**
+   * Create a new Session based on array-data
+   *
+   * @param array $params key-value pairs
+   * @return CRM_Remoteevent_DAO_Session|NULL
+   *
+  public static function create($params) {
+    $className = 'CRM_Remoteevent_DAO_Session';
+    $entityName = 'Session';
+    $hook = empty($params['id']) ? 'create' : 'edit';
+
+    CRM_Utils_Hook::pre($hook, $entityName, CRM_Utils_Array::value('id', $params), $params);
+    $instance = new $className();
+    $instance->copyValues($params);
+    $instance->save();
+    CRM_Utils_Hook::post($hook, $entityName, $instance->id, $instance);
+
+    return $instance;
+  } */
+
+}
