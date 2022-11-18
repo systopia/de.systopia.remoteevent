1e419e04d4f25c131b611ff323386205f76e83e5
Philipp Batroff <batroff@systopia.de>
2022-11-18 17:27:09 +0100

    [#33] Add migration function in upgrader
    Creates new custom fields and migrates currently configured profiled to match new IDs (og-option_value_id)

--- CRM/Remoteevent/Upgrader.php
+++ CRM/Remoteevent/Upgrader.php
@@ -151,6 +151,18 @@ class CRM_Remoteevent_Upgrader extends CRM_Remoteevent_Upgrader_Base
     }
 
     /**
+     * @return TRUE on success
+     * @throws Exception
+     */
+    public function upgrade0012()
+    {
+        $this->ctx->log->info('Updating RegistrationProfile data structures');
+        $customData = new CRM_Remoteevent_CustomData(E::LONG_NAME);
+        $customData->syncCustomGroup(E::path('resources/custom_group_remote_registration.json'));
+        $this->migrate_profiles();
+    }
+    
+    /**
      * Adding configurable XCM profiles
      *
      * @see https://github.com/systopia/de.systopia.remoteevent/issues/25
@@ -171,11 +183,95 @@ class CRM_Remoteevent_Upgrader extends CRM_Remoteevent_Upgrader_Base
     }
 
 
-
-
     /****************************************************************
-    **                       HELPER FUNCTIONS                      **
-    ****************************************************************/
+     **                       HELPER FUNCTIONS                      **
+     ****************************************************************/
+
+    /**
+     * Migrate previous profiles to the new columns:
+     * default_profile -> default_profile_generic
+     * profiles -> profiles_generic
+     * default_update_profile -> default_update_profile_generic
+     * update_profiles -> update_profiles_generic
+     *
+     *
+     * @return void
+     */
+    protected function migrate_profiles()
+    {
+        // first copy existing values
+        CRM_Core_DAO::executeQuery(
+            "UPDATE civicrm_value_remote_registration SET default_profile_generic=default_profile;"
+        );
+        CRM_Core_DAO::executeQuery("UPDATE civicrm_value_remote_registration SET profiles_generic=profiles;");
+        CRM_Core_DAO::executeQuery(
+            "UPDATE civicrm_value_remote_registration SET default_update_profile_generic=default_update_profile;"
+        );
+        CRM_Core_DAO::executeQuery(
+            "UPDATE civicrm_value_remote_registration SET update_profiles_generic=update_profiles;"
+        );
+
+        // Add option value prefix to IDs
+        $optionValues = \Civi\Api4\OptionValue::get()
+            ->addSelect('id', 'value')
+            ->addWhere('option_group_id:name', '=', 'remote_registration_profiles')
+            ->execute();
+        $values = [];
+        foreach ($optionValues as $optionValue) {
+            // do something
+            $values[$optionValue['value']] = "og-" . $optionValue['id'];
+        }
+        // Replace current values
+        $all_event_registration_profiles = CRM_Core_DAO::executeQuery(
+            "
+            SELECT
+                id, default_profile_generic, profiles_generic, default_update_profile_generic,  update_profiles_generic
+            FROM civicrm_value_remote_registration"
+        );
+        $update_values = [];
+        while ($all_event_registration_profiles->fetch()) {
+            $update_values[$all_event_registration_profiles->id] = [];
+            $update_values[$all_event_registration_profiles->id]['default_profile_generic'] = $values[$all_event_registration_profiles->default_profile_generic];
+            $update_values[$all_event_registration_profiles->id]['default_update_profile_generic'] = $values[$all_event_registration_profiles->default_update_profile_generic];
+            $update_values[$all_event_registration_profiles->id]['profiles_generic'] = parse_profiles(
+                $values,
+                $all_event_registration_profiles->profiles_generic
+            );
+            $update_values[$all_event_registration_profiles->id]['update_profiles_generic'] = parse_profiles(
+                $values,
+                $all_event_registration_profiles->update_profiles_generic
+            );
+        }
+        foreach ($update_values as $id => $value) {
+            // update current values
+            CRM_Core_DAO::executeQuery(
+                "
+            UPDATE civicrm_value_remote_registration
+            SET
+                default_profile_generic = '{$value['default_profile_generic']}',
+                default_update_profile_generic = '{$value['default_update_profile_generic']}',
+                profiles_generic = '{$value['profiles_generic']}',
+                update_profiles_generic = '{$value['update_profiles_generic']}'
+            WHERE id = '{$id}';
+        "
+            );
+        }
+    }
+
+    /**
+     * @param $mapping
+     * @param $profiles
+     *
+     * @return mixed
+     */
+    protected function parse_profiles($mapping, $profiles)
+    {
+        $profile_ids = \CRM_Utils_Array::explodePadded($profiles);
+        foreach ($profile_ids as $key => $profile_id) {
+            $profile_ids[$key] = $mapping[$profile_id];
+        }
+        return \CRM_Utils_Array::implodePadded($profile_ids);
+    }
 
     /**
      * Add a unique index on the event external identifiers
@@ -184,11 +280,17 @@ class CRM_Remoteevent_Upgrader extends CRM_Remoteevent_Upgrader_Base
     {
         // gather field / group info
         $external_identifier_field = CRM_Remoteevent_CustomData::getCustomField(
-            'event_remote_registration', 'remote_registration_external_identifier');
+            'event_remote_registration',
+            'remote_registration_external_identifier'
+        );
         if (empty($external_identifier_field)) {
-            throw new Exception("Field 'event_remote_registration.remote_registration_external_identifier' does not exist!");
+            throw new Exception(
+                "Field 'event_remote_registration.remote_registration_external_identifier' does not exist!"
+            );
         }
-        $external_identifier_group = CRM_Remoteevent_CustomData::getGroupSpecs($external_identifier_field['custom_group_id']);
+        $external_identifier_group = CRM_Remoteevent_CustomData::getGroupSpecs(
+            $external_identifier_field['custom_group_id']
+        );
 
         // add unique key (if not already there)
         $find_index_query = "SHOW INDEX FROM `{$external_identifier_group['table_name']}`
@@ -196,9 +298,11 @@ class CRM_Remoteevent_Upgrader extends CRM_Remoteevent_Upgrader_Base
         $index = CRM_Core_DAO::executeQuery($find_index_query);
         if (!$index->fetch()) {
             // index missing: add unique key
-            CRM_Core_DAO::executeQuery("
+            CRM_Core_DAO::executeQuery(
+                "
                 ALTER TABLE `{$external_identifier_group['table_name']}`
-                ADD UNIQUE KEY `UI_external_identifier` (`{$external_identifier_field['column_name']}`)");
+                ADD UNIQUE KEY `UI_external_identifier` (`{$external_identifier_field['column_name']}`)"
+            );
         }
     }
 
@@ -219,7 +323,9 @@ class CRM_Remoteevent_Upgrader extends CRM_Remoteevent_Upgrader_Base
         // check if it's already there
         $apiResult = civicrm_api3('ParticipantStatusType', 'get', ['name' => $name]);
         if ($apiResult['count'] == 0) {
-            $max_weight = (int) CRM_Core_DAO::singleValueQuery("SELECT MAX(weight) FROM civicrm_participant_status_type");
+            $max_weight = (int)CRM_Core_DAO::singleValueQuery(
+                "SELECT MAX(weight) FROM civicrm_participant_status_type"
+            );
             civicrm_api3(
                 'ParticipantStatusType',
                 'create',
