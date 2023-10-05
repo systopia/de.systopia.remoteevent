aec0af0d275ebff8d16a0484086af3cd0bbd1c00
Jens Schuppe <schuppe@systopia.de>
2023-10-05 15:35:13 +0200

    Move retrieving additional participants' fields into a separate method

--- CRM/Remoteevent/RegistrationProfile.php
+++ CRM/Remoteevent/RegistrationProfile.php
@@ -84,6 +84,45 @@ abstract class CRM_Remoteevent_RegistrationProfile
      */
     abstract public function getFields($locale = null);
 
+    public function getAdditionalParticipantsFields(array $event, ?int $maxParticipants = NULL, ?string $locale = NULL): ?array {
+      if (!empty($event['is_multiple_registrations'])) {
+        $fields = [];
+        if (!isset($maxParticipants)) {
+          $maxParticipants = $event['max_additional_participants'];
+        }
+        else {
+          $maxParticipants = min($maxParticipants, $event['max_additional_participants']);
+        }
+        $additional_participants_profile = CRM_Remoteevent_RegistrationProfile::getRegistrationProfile(
+          $event['event_remote_registration.remote_registration_additional_participants_profile']
+        );
+        $additional_fields = $additional_participants_profile->getFields($locale);
+        $fields['additional_participants'] = [
+          'type' => 'fieldset',
+          'name' => 'additional_participants',
+          'label' => E::ts('Additional Participants'),
+          'weight' => 1000,
+          'description' => E::ts('Register up to %1 additional participants', [1 => $event['max_additional_participants']]),
+        ];
+        for ($i = 1; $i <= $maxParticipants; $i++) {
+          $fields['additional_' . $i] = [
+            'type' => 'fieldset',
+            'name' => 'additional_' . $i,
+            'parent' => 'additional_participants',
+            'label' => E::ts('Additional Participant %1', [1 => $i]),
+            'weight' => 10,
+            'description' => E::ts('Registration data for additional participant %1', [1 => $i]),
+          ];
+          foreach ($additional_fields as $additional_field_name => $additional_field) {
+            $additional_field['name'] = 'additional_' . $i . '_' . $additional_field['name'];
+            $additional_field['parent'] = empty($additional_field['parent']) ? 'additional_' . $i : 'additional_' . $i . '_' . $additional_field['parent'];
+            $fields['additional_' . $i . '_' . $additional_field_name] = $additional_field;
+          }
+        }
+      }
+      return $fields ?? NULL;
+    }
+
     /**
      * Add the default values to the form data, so people using this profile
      *  don't have to enter everything themselves
@@ -223,40 +262,11 @@ abstract class CRM_Remoteevent_RegistrationProfile
     {
         // simply add the fields from the profile
         $profile = self::getProfile($get_form_results);
+        $event = $get_form_results->getEvent();
 
         // add the fields
         $locale = $get_form_results->getLocale();
-        $fields = $profile->getFields($locale);
-        // Add fields for additional participants' profile.
-        $event = $get_form_results->getEvent();
-        if (!empty($event['is_multiple_registrations'])) {
-            $additional_participants_profile = CRM_Remoteevent_RegistrationProfile::getRegistrationProfile(
-                $event['event_remote_registration.remote_registration_additional_participants_profile']
-            );
-            $additional_fields = $additional_participants_profile->getFields($locale);
-            $fields['additional_participants'] = [
-              'type' => 'fieldset',
-              'name' => 'additional_participants',
-              'label' => E::ts('Additional Participants'),
-              'weight' => 1000,
-              'description' => E::ts('Register up to %1 additional participants', [1 => $event['max_additional_participants']]),
-            ];
-            for ($i = 1; $i <= $event['max_additional_participants']; $i++) {
-                $fields['additional_' . $i] = [
-                    'type' => 'fieldset',
-                    'name' => 'additional_' . $i,
-                    'parent' => 'additional_participants',
-                    'label' => E::ts('Additional Participant %1', [1 => $i]),
-                    'weight' => 10,
-                    'description' => E::ts('Registration data for additional participant %1', [1 => $i]),
-                ];
-                foreach ($additional_fields as $additional_field_name => $additional_field) {
-                    $additional_field['name'] = 'additional_' . $i . '_' . $additional_field['name'];
-                    $additional_field['parent'] = empty($additional_field['parent']) ? 'additional_' . $i : 'additional_' . $i . '_' . $additional_field['parent'];
-                    $fields['additional_' . $i . '_' . $additional_field_name] = $additional_field;
-                }
-            }
-        }
+        $fields = $profile->getFields($locale) + $profile->getAdditionalParticipantsFields($event, NULL, $locale);
         $get_form_results->addFields($fields);
 
         // add default values
