60a74ac95c014fc68115e2ac5ebdf15bfcf83297
Jens Schuppe <schuppe@systopia.de>
2023-10-26 15:22:30 +0200

    Upgrade civix-generated code to civix version 23.02.1

--- CRM/Remoteevent/Upgrader.php
+++ CRM/Remoteevent/Upgrader.php
@@ -18,7 +18,7 @@ use CRM_Remoteevent_ExtensionUtil as E;
 /**
  * Collection of upgrade steps.
  */
-class CRM_Remoteevent_Upgrader extends CRM_Remoteevent_Upgrader_Base
+class CRM_Remoteevent_Upgrader extends CRM_Extension_Upgrader_Base
 {
 
     /**
