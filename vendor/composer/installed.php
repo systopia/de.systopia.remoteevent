c364fbaa6fa662a2bca572da4fc0ba6271c729a6
Jens Schuppe <schuppe@systopia.de>
2026-03-30 13:38:47 +0200

    Set version to 1.4.0-beta3

---
+++ vendor/composer/installed.php
@@ -0,0 +1,27 @@
+<?php return array(
+    'root' => array(
+        'name' => 'systopia/de.systopia.remoteevent',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'd76c280477c512a31549a69c7b3f2bb2bdbfa76c',
+        'type' => 'civicrm-ext',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(
+            0 => '1.4.x-dev',
+        ),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'systopia/de.systopia.remoteevent' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'd76c280477c512a31549a69c7b3f2bb2bdbfa76c',
+            'type' => 'civicrm-ext',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(
+                0 => '1.4.x-dev',
+            ),
+            'dev_requirement' => false,
+        ),
+    ),
+);
